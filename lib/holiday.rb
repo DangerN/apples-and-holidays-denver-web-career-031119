f3dd02e30dd94d7de80ba6f5e7eed7629903f758
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 22:52:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do | season,holiday |
     holiday.each do |supply|
+      binding.pry
       if supply.include?('BBQ')
         binding.pry
         supplies << holiday
