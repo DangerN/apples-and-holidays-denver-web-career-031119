26814d1422ffe72da9d8170036bb06344eb87181
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 22:22:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,9 +76,9 @@ def all_holidays_with_bbq(holiday_hash)
   supplies = {}
   
   holiday_hash.each do | season,holiday |
-    if holiday_hash[season][holiday].include?("BBQ")
+    #if holiday_hash[season][holiday].include?("BBQ")
     supplies << holiday_hash[season][holiday]
-  end
+  #end
 end
   
   supplies.keys 
