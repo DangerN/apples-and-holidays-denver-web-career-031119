bdd379618deb2feaa76bd626fe3916112936c4f8
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 21:20:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do | season, holiday |
     puts "#{season.to_s.capitalize}:"
-#    season.each do | holiday, supply |
-#      puts "#{holiday.to_s.capitalize}: #{supply.join(', ').capitalize}"
-#    end
+    holiday.each do | holiday, supply |
+      puts "#{holiday.to_s.capitalize}: #{supply.join(', ').capitalize}"
+    end
   end
 
 end
